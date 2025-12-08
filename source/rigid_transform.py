1aaac3a514a5b2e0d50a9dd48f5d7c0a2c136205
Kyle Lucke <luck0692@vandals.uidaho.edu>
2025-12-08 14:13:41 -0700

    add flann_ratio parameter.

--- source/rigid_transform.py
+++ source/rigid_transform.py
@@ -57,15 +57,15 @@ def RANSAC(ps1, ps2, iter_num, min_dis):
     return H, ok
 
 
-def rigid_transform(kp1, dsp1, kp2, dsp2, im1_mask, im2_mask, mode, **kwargs):
+def rigid_transform(kp1, dsp1, kp2, dsp2, im1_mask, im2_mask, mode, flann_ratio=0.4, **kwargs):
     dis = 0.0
     if mode == "d":
         dis = im1_mask.shape[0]
     elif mode == "l" or "r":
         dis = im1_mask.shape[1]
     shifting = (mode, dis)
-
-    X1, X2 = flann_match(kp1, dsp1, kp2, dsp2, ratio=0.4, im1_mask=im1_mask, im2_mask=im2_mask, shifting=shifting, **kwargs)
+    
+    X1, X2 = flann_match(kp1, dsp1, kp2, dsp2, ratio=flann_ratio, im1_mask=im1_mask, im2_mask=im2_mask, shifting=shifting, **kwargs)
     if len(X1) == 0:
         logger.info("len(X1) == 0. Falling back to fast_brief routine.")
         return None, None, None, None
